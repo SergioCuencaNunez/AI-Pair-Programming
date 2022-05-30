be0352be4411581c16cc12d1b69135bdb51fc743
Sergio Cuenca Núñez <sergiocuencanunez@MacBook-Pro-de-Sergio.local>
2022-05-30 15:30:23 +0200

    Clase problemas actualizada

--- problems/problems.py
+++ problems/problems.py
@@ -2,21 +2,17 @@ import problem as pb
 import json
 
 class Problems:
-    def __init__(self, problem):
-        self._problem = problem
-
-    def get_problem(self):
-        return self._problem
+    def __init__(self, problems = []):
+        self._problems = problems
 
     def get_problems(self):
         with open('./data/problems.jsonl', 'r') as json_file:
             problems_json = list(json_file)
 
-        problems = []
-
+        self._problems = []
         for problem_json in problems_json:
             problem = json.loads(problem_json)
             problem_class = pb.Problem(problem['task_id'],problem['prompt'],problem['canonical_solution'],problem['entry_point'])
-            problems.append(problem_class)
+            self._problems.append(problem_class)
         
-        return problems
+        return self._problems
