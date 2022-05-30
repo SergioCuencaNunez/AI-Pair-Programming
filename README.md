d5d0e55d8453a74862510165e464a194e204178a
Cuenca <sergio.cuenca@accenture.com>
2022-05-30 11:13:43 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Prueba1
+# AI Pair Programming
+
+## Lunes, 30 de mayo 2022
+
+- Creación placeholders I/O: modelo, problemas y tests unitarios.
--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-s = pd.Series([1,3,4,5,np.nan,6,8])
-a = 10
-b = 8
-print(a+b)
-print(np.power(a,b))
-print(s)
