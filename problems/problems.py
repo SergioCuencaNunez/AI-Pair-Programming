c530eadf5d496093ff3f814a73fc0c83cc9a88ea
Sergio Cuenca Núñez <sergiocuencanunez@MacBook-Pro-de-Sergio.local>
2022-05-30 14:53:47 +0200

    Datos añadidos y clase problemas

--- problems/problems.py
+++ problems/problems.py
@@ -1,13 +1,15 @@
 import problem as pb
 import json
 
-with open('./data/problems.jsonl', 'r') as json_file:
-    problems_json = list(json_file)
+def get_problems():
+    with open('./data/problems.jsonl', 'r') as json_file:
+        problems_json = list(json_file)
 
-problems = []
-for problem_json in problems_json:
-    problem = json.loads(problem_json)
-    problem_class = pb.Problem(problem['task_id'],problem['prompt'],problem['canonical_solution'],problem['entry_point'])
-    problems.append(problem_class)
+    problems = []
+
+    for problem_json in problems_json:
+        problem = json.loads(problem_json)
+        problem_class = pb.Problem(problem['task_id'],problem['prompt'],problem['canonical_solution'],problem['entry_point'])
+        problems.append(problem_class)
     
-    
+    return problems
