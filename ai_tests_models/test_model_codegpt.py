0962aa697aa627337d7c1cf65b25eba8c946bb1e
Cuenca <sergio.cuenca@accenture.com>
2022-06-03 14:28:43 +0200

    Modelos de prueba añadidos

---
+++ ai_tests_models/test_model_codegpt.py
@@ -0,0 +1,12 @@
+from transformers import pipeline, set_seed
+import json
+
+generator = pipeline('text-generation', model='microsoft/CodeGPT-small-py')
+generator_adapted = pipeline('text-generation', model='microsoft/CodeGPT-small-py-adaptedGPT2')
+set_seed(42)
+text = "def sum_tree(root): '''Given the root of a tree, return the sum of all its nodes'''"
+response = generator(text, max_length=200, num_return_sequences=1)
+responses_a = generator_adapted(text, max_length=200, num_return_sequences=10)
+print(response)
+for response_a in responses_a:
+    print(response_a)
