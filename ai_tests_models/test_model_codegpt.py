f2b3740822a9af1a58661b5442738c38f21959a4
Cuenca <sergio.cuenca@accenture.com>
2022-06-06 12:05:44 +0200

    Test models actualizados

--- ai_tests_models/test_model_codegpt.py
+++ ai_tests_models/test_model_codegpt.py
@@ -1,12 +1,15 @@
 from transformers import pipeline, set_seed
 import json
 
-generator = pipeline('text-generation', model='microsoft/CodeGPT-small-py')
-generator_adapted = pipeline('text-generation', model='microsoft/CodeGPT-small-py-adaptedGPT2')
+generator = pipeline('text-generation', model='microsoft/CodeGPT-small-py', device=0)
+generator_adapted = pipeline('text-generation', model='microsoft/CodeGPT-small-py-adaptedGPT2', device=0)
+
 set_seed(42)
 text = "def sum_tree(root): '''Given the root of a tree, return the sum of all its nodes'''"
+
 response = generator(text, max_length=200, num_return_sequences=1)
 responses_a = generator_adapted(text, max_length=200, num_return_sequences=10)
+
 print(response)
 for response_a in responses_a:
     print(response_a)
--- ai_tests_models/test_model_gpt_neo.py
+++ ai_tests_models/test_model_gpt_neo.py
@@ -1,3 +1,3 @@
 from transformers import pipeline
 generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
-generator("EleutherAI has", do_sample=True, min_length=50)
+print(generator("EleutherAI has", do_sample=True, min_length=50))
