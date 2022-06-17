2ca520e381f122bdde6641b173e01570e05ec28b
Sergio Cuenca Núñez <sergiocuencanunez@MacBook-Pro-de-Sergio.local>
2022-06-17 14:11:46 +0200

    Continuar con cambios en SQL

---
+++ ai_model/model_sql.py
@@ -0,0 +1,16 @@
+import sys,os
+
+BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE)
+
+from ai_model.model import Model
+
+class Model_Dummy(Model):
+    def __init__(self, conf=None):
+        super().__init__(conf)
+    
+    def apply_model(self, problems, solutions):
+        for problem_id, problem in problems.get_problems().items():
+            # Llamada al modelo
+            solution = problem.get_canonical_solution()
+            solutions.add_problem_solution(problem_id, solution)
---
+++ ai_unit_testing/unit_test_sql.py
@@ -0,0 +1,29 @@
+from ai_unit_testing.unit_test import Unit_Test
+import sqlite3
+
+class Unit_Test_Dummy(Unit_Test):
+    def __init__(self, problem_id, test):
+        super().__init__(problem_id, test)
+
+    def apply_test(self, problem, solution):    
+        return self._execute_test(solution.get_solution(), problem.get_canonical_solution(), self.get_test())
+
+    def _execute_test(self, model_solution, canonical_solution, database):
+        try:
+            conn = sqlite3.connect(database)
+            cur = conn.cursor()
+
+            cur.execute(model_solution)
+            model_solution_res = cur.fetchall()
+
+            cur.execute(canonical_solution)
+            canonical_solution_res = cur.fetchall()
+
+            if(model_solution_res == canonical_solution_res):
+                return True
+            else:
+                return False
+            
+        except BaseException:
+            return False
+
