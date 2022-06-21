c589afef107fb0ad962147c743c1cf61c8cb5eaf
Cuenca <sergio.cuenca@accenture.com>
2022-06-21 21:07:21 +0200

    Uso de arquitectura con modelo GPT Neo

---
+++ ai_model/model_dummy.py
@@ -0,0 +1,18 @@
+import sys,os
+
+BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE)
+
+from ai_model.model import Model
+
+class Model_Dummy(Model):
+    def __init__(self, conf=None):
+        super().__init__(conf)
+    
+    def apply_model(self, problems, solutions):
+        for problem_id, problem in problems.get_problems().items():
+            # Llamada al modelo
+            solution = problem.get_canonical_solution()
+
+            for i in range(10):
+                solutions.add_problem_solution(problem_id, solution)
--- ai_model/model_gpt_neo.py
+++ ai_model/model_gpt_neo.py
@@ -10,11 +10,14 @@ class Model_GPT_Neo(Model):
         super().__init__(conf)
 
     def apply_model(self, problems, solutions):
-        from transformers import pipeline
-        model = torch.load(r"C:\Users\sergio.cuenca\Downloads\pytorch_model.bin")
-        model.eval()
+        from transformers import pipeline, AutoConfig
+        config = AutoConfig.from_pretrained('/local/config.json')
+        #model = torch.load(r"/local/pytorch_model.bin", map_location=torch.device('cpu'), config=config)
+        model = torch.hub.load(r'/local/pytorch_model.bin', 'model', from_tf=True, config=config)
+
+        #model.eval()
         #generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
-        #generator = pipeline('text-generation', model = model)
-        #for problem_id, problem in problems.get_problems().items():
-        #    solution_array = generator(problem.get_prompt(), do_sample=True, max_length=200)
-        #    solutions.add_problem_solution(problem_id, solution_array[0]['generated_text'])
+        generator = pipeline('text-generation', model = model)
+        for problem_id, problem in problems.get_problems().items():
+            solution_array = generator(problem.get_prompt(), do_sample=True, max_length=200)
+            solutions.add_problem_solution(problem_id, solution_array[0]['generated_text'])
