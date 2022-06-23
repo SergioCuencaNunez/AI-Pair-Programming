bae2ba752cdde3365b54c0bc341ac51bd821c318
Cuenca <sergio.cuenca@accenture.com>
2022-06-23 12:39:12 +0200

    Uso de arquitectura con modelo GPT Neo

--- ai_model/model_gpt_neo.py
+++ ai_model/model_gpt_neo.py
@@ -10,14 +10,12 @@ class Model_GPT_Neo(Model):
         super().__init__(conf)
 
     def apply_model(self, problems, solutions):
-        from transformers import pipeline, AutoConfig
-        config = AutoConfig.from_pretrained('/local/config.json')
-        #model = torch.load(r"/local/pytorch_model.bin", map_location=torch.device('cpu'), config=config)
-        model = torch.hub.load(r'/local/pytorch_model.bin', 'model', from_tf=True, config=config)
-
-        #model.eval()
-        #generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
-        generator = pipeline('text-generation', model = model)
+        from transformers import GPTNeoConfig, GPTNeoForCausalLM, GPT2Tokenizer 
+        config = GPTNeoConfig.from_pretrained('/local/weights_apps/config.json')
+        model = GPTNeoForCausalLM.from_pretrained('/local/weights_apps/pytorch_model.bin', config=config)
+        tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
         for problem_id, problem in problems.get_problems().items():
-            solution_array = generator(problem.get_prompt(), do_sample=True, max_length=200)
-            solutions.add_problem_solution(problem_id, solution_array[0]['generated_text'])
+            input_ids = torch.LongTensor(tokenizer.encode(problem.get_prompt(), verbose=False)).unsqueeze(0)  
+            output_ids = model.generate(input_ids,num_beams=5,early_stopping=True,max_length=1024 - len(input_ids))
+            output_str = tokenizer.decode(output_ids[0])
+            solutions.add_problem_solution(problem_id, output_str)
